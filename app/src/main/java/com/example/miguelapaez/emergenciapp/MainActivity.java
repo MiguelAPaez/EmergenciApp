1d48918a9b541270eb52faf51381118d08f0d57e
imalready <36923876+imalready@users.noreply.github.com>
2020-03-02 21:47:46 -0500

    Comprobar inicio seccion

--- app/src/main/java/com/example/miguelapaez/emergenciapp/MainActivity.java
+++ app/src/main/java/com/example/miguelapaez/emergenciapp/MainActivity.java
@@ -13,17 +13,19 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser == null){
-        }
-    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
+        //Comprobar Sección
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null){
+            Intent intent = new Intent(MainActivity.this,Login.class);
+            startActivityForResult(intent,0);
+        }
+        else {
+            Log.e("Usuario","Encontrado"+ currentUser.getEmail());
+        }
         setContentView ( R.layout.activity_main );
         getSupportActionBar().hide();
 
