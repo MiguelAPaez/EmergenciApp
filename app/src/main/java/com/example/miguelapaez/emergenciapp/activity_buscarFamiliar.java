d5cb621b3a71a3de8bfc9dea509dccca08118816
alejandro <Alejandro>
2020-03-04 18:51:10 -0500

    pantalla buscar hecha

---
+++ app/src/main/java/com/example/miguelapaez/emergenciapp/activity_buscarFamiliar.java
@@ -0,0 +1,14 @@
+package com.example.miguelapaez.emergenciapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_buscarFamiliar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar_familiar);
+    }
+}
