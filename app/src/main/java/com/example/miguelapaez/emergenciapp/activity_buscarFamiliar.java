3c0ad34d1f6b1d1bc5146586a58ae2f166abad21
miguelapaez <miguelangelpaezramos@gmail.com>
2020-03-05 16:43:12 -0500

    Update BuscarFamiliar
    Arreglos pantalla BuscarFamiliar y algunos cambios más

--- app/src/main/java/com/example/miguelapaez/emergenciapp/activity_buscarFamiliar.java
+++ app/src/main/java/com/example/miguelapaez/emergenciapp/activity_buscarFamiliar.java
@@ -1,10 +1,14 @@
 package com.example.miguelapaez.emergenciapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_buscarFamiliar extends AppCompatActivity {
+
+    TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,11 @@ public class activity_buscarFamiliar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_familiar);
         getSupportActionBar().hide();
+
+        message = (TextView) findViewById ( R.id.messageBuscar );
+        String font_path = "font/Arvo-Regular.ttf";
+        Typeface TF = Typeface.createFromAsset ( getAssets (), font_path );
+        message.setTypeface ( TF );
+
     }
 }
