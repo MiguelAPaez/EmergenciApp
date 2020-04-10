b898d269a81aa0be4382eda60b9db69e09832b62
miguelapaez <miguelangelpaezramos@gmail.com>
2020-04-10 17:34:17 -0500

    Update GridLayout Question
    Cambios en el GridLayout de las preguntas de salud del sistema

--- app/src/main/java/com/example/miguelapaez/emergenciapp/HealthQuestion.java
+++ app/src/main/java/com/example/miguelapaez/emergenciapp/HealthQuestion.java
@@ -1,13 +1,18 @@
 package com.example.miguelapaez.emergenciapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HealthQuestion extends AppCompatActivity {
+
+    GridLayout mainGrid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +20,29 @@ public class HealthQuestion extends AppCompatActivity {
         setContentView ( R.layout.activity_health_question );
         getSupportActionBar().hide();
 
-        LinearLayout pecho = (LinearLayout) findViewById ( R.id.linearLayoutPecho );
-        pecho.setOnClickListener ( new View.OnClickListener () {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent ( v.getContext () , MedicalCenters.class );
-                startActivityForResult ( intent , 0 );
-            }
-        } );
+        mainGrid = (GridLayout) findViewById(R.id.gridLayoutQuestion1);
 
+        setSingleEvent(mainGrid);
     }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            LinearLayout cardView = (LinearLayout) mainGrid.getChildAt( i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText ( view.getContext (), "Seleccionaste a: "
+                            +finalI, Toast.LENGTH_SHORT).show ();
+                    Intent intent = new Intent ( HealthQuestion.this, MedicalCenters.class);
+                    intent.putExtra("info","This is activity from card item index  "+finalI);
+                    startActivity(intent);
+
+                }
+            });
+        }
+    }
+
 }
