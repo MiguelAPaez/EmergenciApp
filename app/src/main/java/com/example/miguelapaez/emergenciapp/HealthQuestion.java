53e87ba5709f784c9ecd421661fefba0e8e3a7ba
miguelapaez <miguelangelpaezramos@gmail.com>
2020-04-16 11:35:50 -0500

    changes

--- app/src/main/java/com/example/miguelapaez/emergenciapp/HealthQuestion.java
+++ app/src/main/java/com/example/miguelapaez/emergenciapp/HealthQuestion.java
@@ -1,48 +1,8 @@
 package com.example.miguelapaez.emergenciapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.GridLayout;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HealthQuestion extends AppCompatActivity {
 
-    GridLayout mainGrid;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_health_question );
-        getSupportActionBar().hide();
-
-        mainGrid = (GridLayout) findViewById(R.id.gridLayoutQuestion1);
-
-        setSingleEvent(mainGrid);
-    }
-
-    private void setSingleEvent(GridLayout mainGrid) {
-        //Loop all child item of Main Grid
-        for (int i = 0; i < mainGrid.getChildCount(); i++) {
-            //You can see , all child item is CardView , so we just cast object to CardView
-            LinearLayout cardView = (LinearLayout) mainGrid.getChildAt(i);
-            final int finalI = i;
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText ( view.getContext (), "Seleccionaste a: "
-                            +finalI, Toast.LENGTH_SHORT).show ();
-                    Intent intent = new Intent ( HealthQuestion.this, HealthQuestionsFace.class);
-                    intent.putExtra("info","This is activity from card item index  "+finalI);
-                    startActivity(intent);
-
-                }
-            });
-        }
-    }
 
 }
